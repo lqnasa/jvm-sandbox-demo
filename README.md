1a8d96cbf797addf3060cb3f93560a9c33b5ec35
R09650\rcd <liqiao@ruijie.com.cn>
2018-09-05 19:18:20 +0800

    M#sandbox#更新优化

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # jvm-sandbox-demo
 阿里巴巴正式开源自研动态非侵入AOP解决方案：JVM-Sandbox  学习demo
+
+
+### JVM-SANDBOX用户手册
+'''
+https://github.com/alibaba/jvm-sandbox/wiki/USER-GUIDE
+
+'''
+
+
+### 使用手册
+'''
+查看class类下对应的方法,暂时不支持静态方法
+./sandbox.sh -p 18982 -d "debug-watch/watch?class=com.ruijie.cvm.task.service.impl.ThriftTaskServiceImpl&method=delImageTask"
+
+查看 sandbox启动模块情况
+./sandbox.sh -p 18982 -l
+
+强制刷新sandbox用户模块 (-F：强制刷新用户模块  -f：刷新用户模块)
+./sandbox.sh -p 18982 -F
+
+-u：卸载指定模块
+./sandbox.sh -p 18982 -u "debug-watch"
+
+-m：查看模块详细信息
+./sandbox.sh -p 18982 -m "debug-watch"
+
+'''
+
+
--- src/main/java/com/lee/DebugWatchRCDModule.java
+++ src/main/java/com/lee/DebugWatchRCDModule.java
@@ -5,13 +5,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.kohsuke.MetaInfServices;
-
 import com.alibaba.jvm.sandbox.api.Information;
 import com.alibaba.jvm.sandbox.api.Module;
 import com.alibaba.jvm.sandbox.api.http.Http;
@@ -22,86 +19,92 @@ import com.alibaba.jvm.sandbox.api.listener.ext.AdviceListener;
 import com.alibaba.jvm.sandbox.api.listener.ext.EventWatchBuilder;
 import com.alibaba.jvm.sandbox.api.listener.ext.EventWatcher;
 import com.alibaba.jvm.sandbox.api.resource.ModuleEventWatcher;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @MetaInfServices(Module.class)
 @Information(id = "debug-watch", version = "0.0.1", author = "liqiao")
 public class DebugWatchRCDModule implements Module {
 
-	@Resource
-	private ModuleEventWatcher moduleEventWatcher;
+    @Resource
+    private ModuleEventWatcher moduleEventWatcher;
 
-	@Http("/watch")
-	public void watch(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
-		final String cnPattern = req.getParameter("class");
-		final String mnPattern = req.getParameter("method");
-		final List<Trigger> triggers = Arrays.asList(Trigger.BEFORE, Trigger.RETURN, Trigger.THROWS);
-		final Printer printer = new ConcurrentLinkedQueuePrinter(resp.getWriter());
-		debugWatch(cnPattern, mnPattern, triggers, printer);
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-	}
+    @Http("/watch")
+    public void watch(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        final String cnPattern = req.getParameter("class");
+        final String mnPattern = req.getParameter("method");
+        final List<Trigger> triggers = Arrays.asList(Trigger.BEFORE, Trigger.RETURN, Trigger.THROWS);
+        final Printer printer = new ConcurrentLinkedQueuePrinter(resp.getWriter());
+        debugWatch(cnPattern, mnPattern, triggers, printer);
 
-	private void debugWatch(final String cnPattern, final String mnPattern, final List<Trigger> triggers,
-			final Printer printer) {
-		EventWatcher watcher = null;
-		try {
-			watcher = new EventWatchBuilder(moduleEventWatcher).onClass(cnPattern).includeSubClasses()
-					.onBehavior(mnPattern).onWatching().withProgress(new ProgressPrinter(printer))
-					.onWatch(new AdviceListener() {
+    }
 
-						@Override
-						public void before(final Advice advice) {
-							printlnByExpress(binding(advice));
-						}
+    private void debugWatch(final String cnPattern, final String mnPattern, final List<Trigger> triggers,
+            final Printer printer) {
+        EventWatcher watcher = null;
+        try {
+            watcher = new EventWatchBuilder(moduleEventWatcher).onClass(cnPattern).includeSubClasses()
+                    .onBehavior(mnPattern).onWatching().withProgress(new ProgressPrinter(printer))
+                    .onWatch(new AdviceListener() {
 
-						@Override
-						public void afterReturning(final Advice advice) {
-							printlnByExpress(binding(advice).bind("return", advice.getReturnObj()));
-						}
+                        @Override
+                        public void before(final Advice advice) {
+                            printlnByExpress(binding(advice));
+                        }
 
-						@Override
-						public void afterThrowing(final Advice advice) {
-							printlnByExpress(binding(advice).bind("throws", advice.getThrowable()));
-						}
+                        @Override
+                        public void afterReturning(final Advice advice) {
+                            String returnObj = gson.toJson(advice.getReturnObj());
+                            printlnByExpress(binding(advice).bind("return", returnObj));
+                        }
 
-						private Bind binding(Advice advice) {
-							return new Bind().bind("class", advice.getBehavior().getDeclaringClass())
-									.bind("method", advice.getBehavior()).bind("params", advice.getParameterArray())
-									.bind("target", advice.getTarget());
-						}
+                        @Override
+                        public void afterThrowing(final Advice advice) {
+                            printlnByExpress(binding(advice).bind("throws", advice.getThrowable()));
+                        }
 
-						private void printlnByExpress(final Bind bind) {
-							String watchObjectStr = bind.entrySet().stream()
-									.map(obj -> obj.getKey() + ":" + obj.getValue()).collect(Collectors.joining(" "));
-							printer.println(watchObjectStr);
-						}
+                        private Bind binding(Advice advice) {
+                            String parameterArray = gson.toJson(advice.getParameterArray());
+                            return new Bind().bind("class", advice.getBehavior().getDeclaringClass())
+                                    .bind("method", advice.getBehavior()).bind("params", parameterArray)
+                                    .bind("target", advice.getTarget());
+                        }
 
-					});
+                        private void printlnByExpress(final Bind bind) {
+                            String watchObjectStr = bind.entrySet().stream()
+                                    .map(obj -> obj.getKey() + ":" + obj.getValue()).collect(Collectors.joining(" "));
+                            printer.println(watchObjectStr);
+                        }
 
-			printer.println(String.format("watching on [%s#%s], at %s.\nPress CTRL_C abort it!", cnPattern, mnPattern,
-					triggers));
-			printer.waitingForBroken();
-		} finally {
-			watcher.onUnWatched();
-		}
-	}
+                    });
 
-	/**
-	 * 观察触点
-	 */
-	enum Trigger {
-		BEFORE, RETURN, THROWS
-	}
+            printer.println(String.format("watching on [%s#%s], at %s.\nPress CTRL_C abort it!", cnPattern, mnPattern,
+                    triggers));
+            printer.waitingForBroken();
+        } finally {
+            watcher.onUnWatched();
+        }
+    }
 
-	class Bind extends HashMap<String, Object> {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 2455316406956962948L;
+    /**
+     * 观察触点
+     */
+    enum Trigger {
+        BEFORE, RETURN, THROWS
+    }
 
-		Bind bind(final String name, final Object value) {
-			put(name, value);
-			return this;
-		}
-	}
+    class Bind extends HashMap<String, Object> {
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 2455316406956962948L;
+
+        Bind bind(final String name, final Object value) {
+            put(name, value);
+            return this;
+        }
+    }
 
 }
